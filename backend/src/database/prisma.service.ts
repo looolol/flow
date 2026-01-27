75c83ec7a99f877946d443e87bb8fb457c9b2fed
Robbie Rankin <robbierankin15@gmail.com>
2026-01-27 04:04:56 -0500

    prisma progress

--- backend/src/database/prisma.service.ts
+++ backend/src/database/prisma.service.ts
@@ -1,8 +1,16 @@
-import { Injectable, OnModuleDestroy, OnModuleInit } from '@nestjs/common';
+import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class PrismaService extends PrismaClient implements OnModuleInit, OnModuleDestroy {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
+  constructor() {
+    console.log("prismaService, process.env.DATABASE_URL", process.env.DATABASE_URL);
+    super();
+  }
+
   async onModuleInit() {
     await this.$connect();
   }

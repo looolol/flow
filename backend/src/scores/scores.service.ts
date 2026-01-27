619800db78b67b459064893bb561d59886492c98
Robbie Rankin <robbierankin15@gmail.com>
2026-01-27 18:59:40 -0500

    added cron job for scores, every 5 min

--- backend/src/scores/scores.service.ts
+++ backend/src/scores/scores.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger } from '@nestjs/common';
 import { NhlApiService } from '../nhl-api/nhl-api.service';
 import { GameWeekDTO } from '@flow/shared';
 import { GameRepository } from '../database/game.repository';
+import { Cron, CronExpression } from '@nestjs/schedule';
 
 @Injectable()
 export class ScoresService {
@@ -11,6 +12,16 @@ export class ScoresService {
     private readonly nhlApi: NhlApiService,
     private readonly games: GameRepository,
   ) {}
+
+  @Cron(CronExpression.EVERY_5_MINUTES)
+  async handleCron() {
+    this.logger.log('Fetching updated scores...');
+    try {
+      await this.getScoreWeek();
+    } catch (error) {
+      this.logger.error('Error fetching scores...', error);
+    }
+  }
 
   async getScoreWeek(): Promise<GameWeekDTO> {
     const schedule = await this.nhlApi.getScheduleToday();
